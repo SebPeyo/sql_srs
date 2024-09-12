7a1ce1261397dccfde94b413013c31117f3d9fed
Sebastien Moreau <sebastien.moreau0209@gmail.com>
2024-09-12 17:13:32 +0200

    display some exercise if no theme selected

--- app.py
+++ app.py
@@ -26,39 +26,43 @@ con = duckdb.connect(database="data/exercises_sql_tables.duckdb", read_only=Fals
 st.write(
     """
 # SQL SRS
-Spaced Repetition System SQL Practice
+##### Spaced Repetition System SQL Practice
 """
 )
 
 # ------------------------------------------------------------
-# Topic Selection
+# Topic and Exercise Selection
 # ------------------------------------------------------------
 
 with st.sidebar:
+    available_themes = (
+        con.execute("SELECT DISTINCT theme FROM memory_state").df().values
+    )
     topic = st.selectbox(
         "What would you like to review?",
-        ("Joins", "GroupBy", "CTEs", "Window Functions"),
+        available_themes,
         index=None,
         placeholder="Select a theme ...",
     )
     if topic:
+        topic_query = f"SELECT * FROM memory_state WHERE theme ={topic}"
         st.write(f"You have selected: {topic}")
+    else:
+        topic_query = "SELECT * FROM memory_state"
 
-        exercise = (
-            con.execute(
-                query="SELECT * FROM memory_state WHERE theme = ?", parameters=(topic,)
-            )
-            .df()
-            .sort_values(by="last_reviewed")
-            .reset_index(drop=True)
-        )
-        st.dataframe(exercise)
+    exercise = (
+        con.execute(query=topic_query)
+        .df()
+        .sort_values(by="last_reviewed")
+        .reset_index(drop=True)
+    )
+    st.dataframe(exercise)
 
-        exercise_name = exercise.loc[0, "exercise_name"]
-        with open(f"answers/{exercise_name}.sql", "r", encoding="utf-8") as f:
-            answer = f.read()
+    exercise_name = exercise.loc[0, "exercise_name"]
+    with open(f"answers/{exercise_name}.sql", "r", encoding="utf-8") as f:
+        answer = f.read()
 
-        df_answer = con.execute(answer).df()
+    df_answer = con.execute(answer).df()
 
 # ------------------------------------------------------------
 # Query input section
@@ -67,33 +71,27 @@ with st.sidebar:
 query = st.text_area(label="Enter your SQL code Here")
 
 if query:
-    if topic:
 
-        df_query = con.execute(query).df()
+    df_query = con.execute(query).df()
 
-        column_diff = df_query.shape[1] - df_answer.shape[1]
-        row_diff = df_query.shape[0] - df_answer.shape[0]
+    column_diff = df_query.shape[1] - df_answer.shape[1]
+    row_diff = df_query.shape[0] - df_answer.shape[0]
 
-        st.dataframe(df_query)
+    st.dataframe(df_query)
 
-        if row_diff != 0:
-            st.write(f"There is a {row_diff} rows difference with the solution!")
+    if row_diff != 0:
+        st.write(f"There is a {row_diff} rows difference with the solution!")
 
-        try:
-            df_query = df_query[df_answer.columns]
+    try:
+        df_query = df_query[df_answer.columns]
 
-            df_compare = df_query.compare(
-                df_answer, result_names=("answer", "solution")
-            )
+        df_compare = df_query.compare(df_answer, result_names=("answer", "solution"))
 
-            if df_compare.shape != (0, 0):
-                st.dataframe(df_compare)
+        if df_compare.shape != (0, 0):
+            st.dataframe(df_compare)
 
-        except KeyError as e:
-            st.write(f"There is a {column_diff} columns difference with the solution!")
-
-    else:
-        st.write("Select a topic first ! 😉")
+    except KeyError as e:
+        st.write(f"There is a {column_diff} columns difference with the solution!")
 
 # ------------------------------------------------------------
 # Tables and Answer section
@@ -102,14 +100,13 @@ if query:
 tab1, tab2 = st.tabs(["Tables", "Answer"])
 
 with tab1:
-    if topic:
-        exercise_tables = exercise.loc[0, "tables"]
 
-        for table in exercise_tables:
-            st.write(f"<u>Table</u>: **{table}**", unsafe_allow_html=True)
-            df = con.execute(query=f"SELECT * FROM {table}").df()
-            st.dataframe(df)
+    exercise_tables = exercise.loc[0, "tables"]
+
+    for table in exercise_tables:
+        st.write(f"<u>Table</u>: **{table}**", unsafe_allow_html=True)
+        df = con.execute(query=f"SELECT * FROM {table}").df()
+        st.dataframe(df)
 
 with tab2:
-    if topic:
-        st.code(answer)
+    st.code(answer)
