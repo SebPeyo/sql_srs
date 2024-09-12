cd06d6bf4936febf8baa41eacc9c83bc49a11ff2
Sebastien Moreau <sebastien.moreau0209@gmail.com>
2024-09-12 18:47:12 +0200

    refactoring of code

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import logging
 import os
 
 import duckdb
+import pandas as pd
 import streamlit as st
 
 # ------------------------------------------------------------
@@ -31,10 +32,39 @@ st.write(
 )
 
 # ------------------------------------------------------------
-# Topic and Exercise Selection
+# Functions
 # ------------------------------------------------------------
 
-with st.sidebar:
+def check_user_solution(user_query: str) -> None:
+    """
+    Check whether the user's SQL query is correct by:
+    1. Checking the number of rows/columns
+    2. Checking the values
+    :param user_query: a string containing the query inserted by the user
+    """
+    df_query = con.execute(user_query).df()
+    column_diff = df_query.shape[1] - df_answer.shape[1]
+    row_diff = df_query.shape[0] - df_answer.shape[0]
+    st.dataframe(df_query)
+    if row_diff != 0:
+        st.write(f"There is a {row_diff} rows difference with the solution!")
+    try:
+        df_query = df_query[df_answer.columns]
+
+        df_compare = df_query.compare(df_answer, result_names=("answer", "solution"))
+
+        if df_compare.shape != (0, 0):
+            st.dataframe(df_compare)
+
+    except KeyError:
+        st.write(f"There is a {column_diff} columns difference with the solution!")
+
+def get_topic() -> str:
+    """
+    Define the SQL topic to train on based on the user input.
+    :return: the name of the topic
+    """
+    global topic
     available_themes = (
         con.execute("SELECT DISTINCT theme FROM memory_state").df().values
     )
@@ -44,26 +74,49 @@ with st.sidebar:
         index=None,
         placeholder="Select a theme ...",
     )
-    if topic:
-        topic_query = f"SELECT * FROM memory_state WHERE theme ={topic}"
-        st.write(f"You have selected: {topic}")
+    return topic
+
+def get_exercise(user_topic: str) -> pd.DataFrame:
+    """
+    Returns a DataFrame with all the available exercises based on the user topic selection.
+    :param user_topic: The topic selected by the user (default None if no topic selected)
+    :return: DataFrame containing the exercises
+    """
+    global exercise
+    if user_topic:
+        topic_query = f"SELECT * FROM memory_state WHERE theme ='{user_topic}'"
+        st.write(f"You have selected: **{user_topic}**")
     else:
         topic_query = "SELECT * FROM memory_state"
-
     exercise = (
         con.execute(query=topic_query)
         .df()
         .sort_values(by="last_reviewed")
         .reset_index(drop=True)
     )
-    st.dataframe(exercise)
+    return exercise
+# ------------------------------------------------------------
+# Topic and Exercise Selection
+# ------------------------------------------------------------
 
-    exercise_name = exercise.loc[0, "exercise_name"]
+def get_solution(exercises: pd.DataFrame) -> str:
+    """
+    Returns the answer of the least recently reviewed exercises
+    :param exercises: DataFrame containing all the exercises for a pre-selected topic
+    :return: A SQL query that solves the exercise
+    """
+    global answer
+    exercise_name = exercises.loc[0, "exercise_name"]
     with open(f"answers/{exercise_name}.sql", "r", encoding="utf-8") as f:
         answer = f.read()
+    return answer
 
+
+with st.sidebar:
+    topic=get_topic()
+    exercise=get_exercise(topic)
+    answer=get_solution(exercise)
     df_answer = con.execute(answer).df()
-
 # ------------------------------------------------------------
 # Query input section
 # ------------------------------------------------------------
@@ -71,27 +124,7 @@ with st.sidebar:
 query = st.text_area(label="Enter your SQL code Here")
 
 if query:
-
-    df_query = con.execute(query).df()
-
-    column_diff = df_query.shape[1] - df_answer.shape[1]
-    row_diff = df_query.shape[0] - df_answer.shape[0]
-
-    st.dataframe(df_query)
-
-    if row_diff != 0:
-        st.write(f"There is a {row_diff} rows difference with the solution!")
-
-    try:
-        df_query = df_query[df_answer.columns]
-
-        df_compare = df_query.compare(df_answer, result_names=("answer", "solution"))
-
-        if df_compare.shape != (0, 0):
-            st.dataframe(df_compare)
-
-    except KeyError as e:
-        st.write(f"There is a {column_diff} columns difference with the solution!")
+    check_user_solution(query)
 
 # ------------------------------------------------------------
 # Tables and Answer section
