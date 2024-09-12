cc406069f78eb82b28300632407a01b2d3a027a9
Sebastien Moreau <sebastien.moreau0209@gmail.com>
2024-09-12 23:09:56 +0200

    added hours and quarters exercise

--- init_db.py
+++ init_db.py
@@ -12,13 +12,30 @@ con = duckdb.connect(database="data/exercises_sql_tables.duckdb", read_only=Fals
 # ------------------------------------------------------------
 
 data = {
-    "theme": ["Joins", "Joins"],
-    "exercise_name": ["beverages_and_food", "sizes_and_trademarks"],
-    "tables": [["beverages", "food_items"], ["sizes", "trademarks"]],
-    "last_reviewed": ["2024-09-12", "2024-09-11"],
+    "theme": [
+        "Joins",
+        "Joins",
+        "Joins"
+    ],
+    "exercise_name": [
+        "beverages_and_food",
+        "sizes_and_trademarks",
+        "hours_and_quarters"
+    ],
+    "tables": [
+        ["beverages", "food_items"],
+        ["sizes", "trademarks"],
+        ["hours", "quarters"]
+    ],
+    "last_reviewed": [
+        "1970-01-01",
+        "1970-01-01",
+        "1970-01-01",
+    ],
     "question": [
         "Do a CROSS JOIN with the two tables.",
         "Do a CROSS JOIN with the two tables.",
+        "Join the two tables to obtain every quarter from 8:00 to 12:45"
     ],
 }
 memory_state_df = pd.DataFrame(data)
@@ -70,5 +87,29 @@ Lewis
 
 trademarks = pd.read_csv(io.StringIO(TRADEMARKS))
 con.execute("CREATE OR REPLACE TABLE trademarks AS SELECT * FROM trademarks")
+
+# ------------------------------------------------------------
+# CROSS JOIN - hours and quarters
+# ------------------------------------------------------------
+HOURS="""
+hour
+8
+9
+10
+11
+12"""
+
+hours = pd.read_csv(io.StringIO(HOURS))
+con.execute("CREATE OR REPLACE TABLE hours AS SELECT * FROM hours")
+
+QUARTERS= """
+quarters
+00
+15
+30
+45"""
+
+quarters = pd.read_csv(io.StringIO(QUARTERS))
+con.execute("CREATE OR REPLACE TABLE quarters AS SELECT * FROM quarters")
 
 con.close()
